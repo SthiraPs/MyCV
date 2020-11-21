253e5ff4c528c1598d20805353aaf32d6b6666f0
SthiraPs <52598866+SthiraPs@users.noreply.github.com>
2020-11-21 23:05:46 +0530

    Edited some parts and added a few more details
    Added a side bar menu instead of nav bar menu.
    Changed colors
    Added some other feachers to the resume

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-$(document).ready(function ()
-{
-    $('.menu-toggler').on('click',function ()
-    {
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
-    })
-});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main-body").style.marginLeft = "250px";
+    document.getElementById("main").style.display = 'none'
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main-body").style.marginLeft= "0";
+    document.getElementById("main").style.display = 'block'
+}
+
