1bfb898301d06cbf00edb45a584cc05c34e65678
SthiraPs <52598866+SthiraPs@users.noreply.github.com>
2020-11-22 12:36:29 +0530

    Changed the background image
    Changed the background image and font colors. Added some more details to the about section.

--- main.js
+++ main.js
@@ -11,3 +11,30 @@ function closeNav() {
     document.getElementById("main").style.display = 'block'
 }
 
+
+$(document).ready(function ()
+{
+    
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    })
+});
+
+var mybutton = document.getElementById("up");
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 8000 || document.documentElement.scrollTop > 8000) {
+    mybutton.style.display = "block";
+  }
+  else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
